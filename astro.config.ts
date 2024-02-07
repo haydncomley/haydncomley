3434b48a37f633123b7e0062caf7aea7e1f23c2f
Haydn Comley <haydncomley@gmail.com>
2024-02-07 01:02:32 +0000

    Remove single file

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
-import astroSingleFile from 'astro-single-file';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     astroI18next(),
-    astroSingleFile(),
   ],
   experimental: {
     viewTransitions: true,
